16634e0e0bd70237c82ffd989cfe78075c251ed8
himanshuapril1 <Himanshuapr1@gmail.com>
2016-04-04 23:47:17 +0530

    Bug fix

--- services/get-news.php
+++ services/get-news.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: text/html; charset=UTF-8");
+header("Content-Type: application/json; charset=UTF-8");
 
 $uri = $_REQUEST['uri'];
 function getPos($a){
